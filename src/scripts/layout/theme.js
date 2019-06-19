6ca270322798eaeb467072718b97ca94ed09bc7f
Donato Amézquita <hola@donato.mx>
2019-06-19 15:17:49 -0500

    Search page logic working Styles and order pending

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -1,3 +1,7 @@
+// Testeo aquí arriba es donde van estos dos, slate los agarra y los compila, abajo no los pelaba, revisar los imports y partials. DXebe estar el theme.scss con los imports bien y estos igualitos al momiji.scss que se ha estado utilizando en todo el desarrollo. Hacer un pagespeed de google para revisar qué sheets de estilo quitamos.
+//import '../../styles/theme.scss';
+//import '../../styles/theme.scss.liquid';
+
 import 'lazysizes/plugins/object-fit/ls.object-fit';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import 'lazysizes/plugins/rias/ls.rias';
@@ -5,8 +9,9 @@ import 'lazysizes/plugins/bgset/ls.bgset';
 import 'lazysizes';
 import 'lazysizes/plugins/respimg/ls.respimg';
 
-import '../../styles/theme.scss';
-import '../../styles/theme.scss.liquid';
+// Testeo quitando estos styles, se pueden activar si algo no funciona
+//import '../../styles/theme.scss';
+//import '../../styles/theme.scss.liquid';
 
 import {focusHash, bindInPageLinks} from '@shopify/theme-a11y';
 import {cookiesEnabled} from '@shopify/theme-cart';
