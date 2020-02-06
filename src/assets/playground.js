7d5e09018a2eefa1bd25301075a721ead0288173
Donato Amézquita <hola@donato.mx>
2020-02-06 15:42:56 -0600

    not working

--- src/assets/playground.js
+++ src/assets/playground.js
@@ -1,6 +1,10 @@
+console.log('fulanito');
+
 // variables
 const imgMercado = document.querySelector('img[alt="Mercado Pago"]');
 const myContainer = document.querySelector('label[for="checkout_payment_gateway_38941884521"]');
+const myWrapper = document.querySelector('payment-method-wrapper');
+console.log(myWrapper);
 
 // replace function
 function replaceGateway() {
@@ -15,6 +19,9 @@ document.onchange = function() {
   const myContainer2 = document.querySelector('label[for="checkout_payment_gateway_38941884521"]');
   imgMercado2.style.display = 'none';
 
-    // se necesita un IF para pintar este texto que sigue, si ya existia, no pintarlo de nuevo....
-  myContainer2.insertAdjacentText('afterbegin', 'Pago con tarjeta de credito, debito, OXXO y SPEI');
+  // se necesita un IF para pintar este texto que sigue, si ya existia, no pintarlo de nuevo....
+  if (myContainer2 !== 'null') {
+    console.log('no existe');
+    myContainer2.insertAdjacentText('afterbegin', 'Pago con tarjeta de credito, debito, OXXO y SPEI');
+  }
 };
