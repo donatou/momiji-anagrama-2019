b4880ecf2c866d3e541c2232eb6f5f906f481f5e
Donato Amézquita <hola@donato.mx>
2020-11-05 13:38:26 -0600

    opacity on refresh

--- src/assets/minicart.js
+++ src/assets/minicart.js
@@ -9,9 +9,7 @@ $(document).on('click', '.cart-summary', function (e) {
 
 $(document).on('click', '.refresh-btn', function (e) {
   e.preventDefault();
-  //$('body').addClass('cart-open');
-  //$('#mm_cartoverlay').animate({ "right": '0' });
-  //$('#cart_temp_late form').css('opacity', '0.2');
+  $('#cart_temp_late form').css('opacity', '0.2');
   updateMinicart();
 });
 
